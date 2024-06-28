da933382e1ea1634275eca1bc9213b43feca5ef3
Valeryio <tommagoma23@gmail.com>
2024-06-28 20:50:53 +0000

    update the name of the package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,22 +1,7 @@
 # This is a puppet file used to install specific packages
-# Variables pour les chemins
-$python3_cmd = '/usr/bin/python3'
-$pip3_cmd = '/usr/bin/pip3'
-
-# Installation de Python 3
-package { 'python3':
-  ensure => installed,
-}
-
-# Installation de pip pour Python 3
-package { 'python3-pip':
-  ensure   => installed,
-  provider => 'apt',
-  require  => Package['python3'],
-}
 
 package { 'install_flask':
-  command => 'pip3 install Flask=2.1.0',
+  command => 'pip3 install flask=2.1.0',
   unless  => '/usr/bin/pip3 show flask',
   require => Package['python3-pip'],
 }
