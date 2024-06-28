d1d56e2512284b9356f3930f2d1aef124c04ebf9
Valeryio <tommagoma23@gmail.com>
2024-06-28 20:13:08 +0000

    commit the task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This is a puppet file that creates a file
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet',
+}
