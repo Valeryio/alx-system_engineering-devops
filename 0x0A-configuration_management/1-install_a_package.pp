f49d75d40bbb8ae33785ef06396fbfcbfd3b45aa
Valeryio <tommagoma23@gmail.com>
2024-06-29 09:24:31 +0000

    update the task 1 this 28 morning

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # This is a puppet file used to install specific packages
 
-exec { 'install_flask':
-  command => 'pip3 install Flask=2.1.0',
-  unless  => '/usr/bin/pip3 show Flask',
-  require => Package['python3-pip'],
+exec { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
