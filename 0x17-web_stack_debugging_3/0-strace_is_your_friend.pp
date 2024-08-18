df176097440c7ed1fdd779379077c8733531f838
Valeryio <tommagoma23@gmail.com>
2024-08-18 16:04:42 +0000

    update the file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,10 @@
-# A puppet script to replace the extension of a file in the wordpress setting file
-$setting_file = ‘/var/www/html/wp-settings.php’
+# A puppet script to replace the extension of a file in the WordPress setting file
+
+$setting_file = '/var/www/html/wp-settings.php'
+
 # replace the class name extension from phpp to php
-exec { ‘replace_class_name’:
- command => “sed -i ‘s/class-wp-locale.phpp/class-wp-locale.php/g’ ${setting_file}”,
- path => [‘/bin’, ‘/usr/bin’]
+exec { 'replace_class_name':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' ${setting_file}",
+  path    => ['/bin', '/usr/bin'],
 }
+
