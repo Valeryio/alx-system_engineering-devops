3891f930303d4fb5b99a71837e9ed71749d6477b
Valeryio <tommagoma23@gmail.com>
2024-08-18 14:38:05 +0000

    Update the task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,31 +9,33 @@ import json
 import requests
 
 
-# requests to get the data from the jsonplaceholder... API
-employee_id = sys.argv[1]
-employee = requests.get("https://jsonplaceholder.typicode.com/users/{}\
+if __name__ == "__main__":
+
+    # requests to get the data from the jsonplaceholder... API
+    employee_id = sys.argv[1]
+    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}\
 ".format(employee_id))
-employee_tasks = requests.get("https://jsonplaceholder.typicode.com/users/\
+    employee_tasks = requests.get("https://jsonplaceholder.typicode.com/users/\
 {}/todos".format(employee_id))
 
-all_tasks = employee_tasks.json()
-tasks_list = []
-json_obj = {}
+    all_tasks = employee_tasks.json()
+    tasks_list = []
+    json_obj = {}
 
-# create the appropriate python object
-for i in range(len(all_tasks)):
-    new_dict = {}
-    new_dict["task"] = all_tasks[i]["title"]
-    new_dict["completed"] = all_tasks[i]["completed"]
-    new_dict["username"] = employee.json().get("username")
+    # create the appropriate python object
+    for i in range(len(all_tasks)):
+        new_dict = {}
+        new_dict["task"] = all_tasks[i]["title"]
+        new_dict["completed"] = all_tasks[i]["completed"]
+        new_dict["username"] = employee.json().get("username")
 
-    tasks_list.append(new_dict)
+        tasks_list.append(new_dict)
 
-# convert the python object to a json file
-json_obj[employee_id] = tasks_list
-json_obj = json.dumps(json_obj)
+    # convert the python object to a json file
+    json_obj[employee_id] = tasks_list
+    json_obj = json.dumps(json_obj)
 
-# write the json object into the file
-filename = str(employee_id) + ".json"
-with open(filename, "w") as file:
-    file.write(json_obj)
+    # write the json object into the file
+    filename = str(employee_id) + ".json"
+    with open(filename, "w") as file:
+        file.write(json_obj)
