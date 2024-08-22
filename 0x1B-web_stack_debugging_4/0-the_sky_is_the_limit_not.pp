d29bafd940e4355822d799635bb283a42f320e48
Valeryio <tommagoma23@gmail.com>
2024-08-22 19:58:42 +0000

    update on the task 000

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -9,3 +9,8 @@ exec { 'nginx-fail-fixing':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:bin/',
 }
+
+exec { 'restart-the-engine':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
