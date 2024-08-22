29d45f27280caca4341bfb7caf9ee706800b79ad
Valeryio <tommagoma23@gmail.com>
2024-08-22 20:14:53 +0000

    update task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,11 @@
 # Another script
 
-exec { 'ulimit':
-  command => 'ulimit -n 4096'
+exec { 'ulimit-hard':
+  command => 'sed -i "/^holberton hard/s/5/5000" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
+exec { 'ulimit-soft':
+  command => 'sed -i "/^holberton soft/s/5/5000" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
